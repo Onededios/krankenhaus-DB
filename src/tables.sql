80b15093703822424645291d15b682926be15dd8
Onededios <joel10olor@gmail.com>
2023-05-18 23:36:26 +0200

    updated the tables and roles

--- src/tables.sql
+++ src/tables.sql
@@ -1,123 +1,101 @@
-CREATE  TABLE krankenhaus_schema.ciutat (
-	idciutat             SERIAL PRIMARY KEY  ,
-	nomciutat            text    ,
-	CONSTRAINT unq_ciutat_nomciutat UNIQUE ( nomciutat )
- );
+CREATE TABLE krankenhaus_schema.ciutat (
+    idciutat SERIAL PRIMARY KEY,
+    nomciutat TEXT,
+    CONSTRAINT unq_ciutat_nomciutat UNIQUE (nomciutat)
+);
 
-CREATE  TABLE krankenhaus_schema.hospital (
-	idhospital           SERIAL PRIMARY KEY  ,
-	nomhospital          text  NOT NULL  ,
-	numllitsucilliures   bigint    ,
-	idciutat             bigint    ,
-	CONSTRAINT unq_hospital_nomhospital UNIQUE ( nomhospital )  ,
+CREATE TABLE krankenhaus_schema.hospital (
+    idhospital SERIAL PRIMARY KEY,
+    nomhospital TEXT NOT NULL,
+    numllitsucilliures BIGINT,
+    idciutat BIGINT,
+    CONSTRAINT unq_hospital_nomhospital UNIQUE (nomhospital),
     CONSTRAINT fk_hospital_ciutat FOREIGN KEY (idciutat) REFERENCES krankenhaus_schema.ciutat (idciutat) ON DELETE CASCADE
- );
+);
 
-ALTER TABLE krankenhaus_schema.hospital ADD CONSTRAINT fk_hospital_ciutat_0 FOREIGN KEY ( idciutat ) REFERENCES krankenhaus_schema.ciutat ( idciutat );
+CREATE TABLE krankenhaus_schema.persona (
+    idpersona SERIAL PRIMARY KEY,
+    idhospital BIGINT NOT NULL,
+    nom TEXT NOT NULL,
+    cognom TEXT NOT NULL,
+    datanaixement DATE,
+    CONSTRAINT fk_persona_hospital FOREIGN KEY (idhospital) REFERENCES krankenhaus_schema.hospital (idhospital) ON DELETE CASCADE
+);
 
-CREATE  TABLE krankenhaus_schema.persona (
-	idpersona            SERIAL PRIMARY KEY  ,
-    idhospital           bigint  NOT NULL  ,
-	nom                  text  NOT NULL  ,
-	cognom               text  NOT NULL  ,
-	datanaixement        date
- );
-
-ALTER TABLE krankenhaus_schema.persona ADD CONSTRAINT fk_persona_hospital FOREIGN KEY ( idhospital ) REFERENCES krankenhaus_schema.hospital ( idhospital );
-
-CREATE  TABLE krankenhaus_schema.pacient (
-	idpacient            SERIAL  PRIMARY KEY  ,
-	idpersona            bigint  NOT NULL  ,
-	planta               text    ,
-	habitacio            text    ,
-	ocupauci             boolean    ,
-	estat                text    ,
-	CONSTRAINT unq_pacient_idpersona UNIQUE ( idpersona ) ,
+CREATE TABLE krankenhaus_schema.pacient (
+    idpacient SERIAL PRIMARY KEY,
+    idpersona BIGINT NOT NULL,
+    planta TEXT,
+    habitacio TEXT,
+    ocupauci BOOLEAN,
+    estat TEXT,
+    CONSTRAINT unq_pacient_idpersona UNIQUE (idpersona),
     CONSTRAINT fk_pacient_persona FOREIGN KEY (idpersona) REFERENCES krankenhaus_schema.persona (idpersona) ON DELETE CASCADE
- );
+);
 
-ALTER TABLE krankenhaus_schema.pacient ADD CONSTRAINT fk2_pacient_persona FOREIGN KEY ( idpersona ) REFERENCES krankenhaus_schema.persona ( idpersona );
+CREATE TABLE krankenhaus_schema.cartillavacunes (
+    idcartilla SERIAL PRIMARY KEY,
+    idpacient BIGINT NOT NULL,
+    idvacuna BIGINT NOT NULL,
+    datavacunacio DATE NOT NULL,
+    data2vacunacio DATE,
+    date3vacunacio DATE,
+    CONSTRAINT fk_cartillavacunes_pacient FOREIGN KEY (idpacient) REFERENCES krankenhaus_schema.pacient (idpacient) ON DELETE CASCADE,
+    CONSTRAINT fk_cartillavacunes_vacuna FOREIGN KEY (idvacuna) REFERENCES krankenhaus_schema.vacuna (idvacuna) ON DELETE CASCADE
+);
 
-CREATE  TABLE krankenhaus_schema.cartillavacunes (
-	idcartilla           SERIAL  PRIMARY KEY  ,
-	idpacient            bigint  NOT NULL  ,
-	idvacuna             bigint  NOT NULL  ,
-	datavacunacio        date  NOT NULL  ,
-	data2vacunacio       date ,
-	date3vacunacio       date ,
-    CONSTRAINT fk_pacient_cartilla FOREIGN KEY (idpacient) REFERENCES krankenhaus_schema.pacient (idpacient) ON DELETE CASCADE
- );
+CREATE TABLE krankenhaus_schema.vacuna (
+    idvacuna SERIAL PRIMARY KEY,
+    nomvacuna TEXT NOT NULL,
+    laboratori TEXT,
+    numdosis DECIMAL NOT NULL,
+    CONSTRAINT unq_vacuna_nomvacuna UNIQUE (nomvacuna)
+);
 
-ALTER TABLE krankenhaus_schema.cartillavacunes ADD CONSTRAINT fk_cartillavacunes_pacient_0 FOREIGN KEY ( idpacient ) REFERENCES krankenhaus_schema.pacient ( idpacient );
-
-CREATE  TABLE krankenhaus_schema.vacuna (
-	idvacuna             SERIAL PRIMARY KEY  ,
-	nomvacuna            text  NOT NULL  ,
-	laboratori           text    ,
-	numdosis             decimal  NOT NULL  ,
-	CONSTRAINT unq_vacuna_nomvacuna UNIQUE ( nomvacuna )
- );
-
-ALTER TABLE krankenhaus_schema.cartillavacunes ADD CONSTRAINT fk_cartillavacunes_vacuna_0 FOREIGN KEY ( idvacuna ) REFERENCES krankenhaus_schema.vacuna ( idvacuna );
-
-CREATE  TABLE krankenhaus_schema.virus (
-	idvirus              SERIAL  PRIMARY KEY  ,
-	idvacuna             bigint  NOT NULL  ,
-	nomvirus             text    ,
-	numvariant           integer    ,
+CREATE TABLE krankenhaus_schema.virus (
+    idvirus SERIAL PRIMARY KEY,
+    idvacuna BIGINT NOT NULL,
+    nomvirus TEXT,
+    numvariant INTEGER,
     CONSTRAINT fk_virus_vacuna FOREIGN KEY (idvacuna) REFERENCES krankenhaus_schema.vacuna (idvacuna) ON DELETE CASCADE
- );
+);
 
-ALTER TABLE krankenhaus_schema.virus ADD CONSTRAINT fk_virus_vacuna_0 FOREIGN KEY ( idvacuna ) REFERENCES krankenhaus_schema.vacuna ( idvacuna );
+CREATE TABLE krankenhaus_schema.stock (
+    idvacuna BIGINT NOT NULL,
+    idhospital BIGINT NOT NULL,
+    qty BIGINT NOT NULL,
+    CONSTRAINT fk_stock_vacuna FOREIGN KEY (idvacuna) REFERENCES krankenhaus_schema.vacuna (idvacuna),
+    CONSTRAINT fk_stock_hospital FOREIGN KEY (idhospital) REFERENCES krankenhaus_schema.hospital (idhospital)
+);
 
-CREATE  TABLE krankenhaus_schema.stock (
-	idvacuna             bigint  NOT NULL  ,
-	idhospital           bigint  NOT NULL  ,
-	qty                  bigint  NOT NULL
- );
-
-ALTER TABLE krankenhaus_schema.stock ADD CONSTRAINT fk_stock_vacuna_0 FOREIGN KEY ( idvacuna ) REFERENCES krankenhaus_schema.vacuna ( idvacuna );
-
-ALTER TABLE krankenhaus_schema.stock ADD CONSTRAINT fk_stock_hospital_0 FOREIGN KEY ( idhospital ) REFERENCES krankenhaus_schema.hospital ( idhospital );
-
-CREATE  TABLE krankenhaus_schema.treballador (
-	idtreballador        SERIAL  PRIMARY KEY  ,
-	idpersona            bigint  NOT NULL  ,
+CREATE TABLE krankenhaus_schema.treballador (
+    idtreballador SERIAL PRIMARY KEY,
+    idpersona BIGINT NOT NULL,
     CONSTRAINT fk_treballador_persona FOREIGN KEY (idpersona) REFERENCES krankenhaus_schema.persona (idpersona) ON DELETE CASCADE
- );
+);
 
-ALTER TABLE krankenhaus_schema.treballador ADD CONSTRAINT fk_treballador_persona_0 FOREIGN KEY ( idpersona ) REFERENCES krankenhaus_schema.persona ( idpersona );
+CREATE TABLE krankenhaus_schema.doctor (
+    iddoctor SERIAL PRIMARY KEY,
+    idtreballador BIGINT NOT NULL,
+    idpacient BIGINT,
+    especialitat TEXT,
+    CONSTRAINT fk_doctor_treballador FOREIGN KEY (idtreballador) REFERENCES krankenhaus_schema.treballador (idtreballador) ON DELETE CASCADE,
+    CONSTRAINT fk_doctor_pacient FOREIGN KEY (idpacient) REFERENCES krankenhaus_schema.pacient (idpacient) ON DELETE CASCADE
+);
 
-CREATE  TABLE krankenhaus_schema.doctor (
-	iddoctor             SERIAL PRIMARY KEY  ,
-	idtreballador        bigint  NOT NULL  ,
-	idpacient            bigint    ,
-	especialitat         text    ,
-    CONSTRAINT fk_doctor_treballador FOREIGN KEY (idtreballador) REFERENCES krankenhaus_schema.treballador (idtreballador) ON DELETE CASCADE
- );
+CREATE TABLE krankenhaus_schema.enfermer (
+    idenfermer SERIAL PRIMARY KEY,
+    idtreballador BIGINT NOT NULL,
+    idpacient BIGINT,
+    especilitat TEXT,
+    CONSTRAINT unq_enfermer_idpacient UNIQUE (idpacient),
+    CONSTRAINT fk_enfermer_treballador FOREIGN KEY (idtreballador) REFERENCES krankenhaus_schema.treballador (idtreballador) ON DELETE CASCADE,
+    CONSTRAINT fk_enfermer_pacient FOREIGN KEY (idpacient) REFERENCES krankenhaus_schema.pacient (idpacient) ON DELETE CASCADE
+);
 
-ALTER TABLE krankenhaus_schema.doctor ADD CONSTRAINT fk_doctor_treballador_0 FOREIGN KEY ( idtreballador ) REFERENCES krankenhaus_schema.treballador ( idtreballador );
-
-ALTER TABLE krankenhaus_schema.doctor ADD CONSTRAINT fk_doctor_pacient_0 FOREIGN KEY ( idpacient ) REFERENCES krankenhaus_schema.pacient ( idpacient );
-
-CREATE  TABLE krankenhaus_schema.enfermer (
-	idenfermer           SERIAL PRIMARY KEY  ,
-	idtreballador        bigint  NOT NULL  ,
-	idpacient            bigint    ,
-	especilitat          text    ,
-	CONSTRAINT unq_enfermer_idpacient UNIQUE ( idpacient )  ,
-    CONSTRAINT fk_enfermer_treballador FOREIGN KEY (idtreballador) REFERENCES krankenhaus_schema.treballador (idtreballador) ON DELETE CASCADE
- );
-
-ALTER TABLE krankenhaus_schema.enfermer ADD CONSTRAINT fk_enfermer_treballador_0 FOREIGN KEY ( idtreballador ) REFERENCES krankenhaus_schema.treballador ( idtreballador );
-
-ALTER TABLE krankenhaus_schema.enfermer ADD CONSTRAINT fk_enfermer_pacient_0 FOREIGN KEY ( idpacient ) REFERENCES krankenhaus_schema.pacient ( idpacient );
-
-CREATE  TABLE krankenhaus_schema.administratiu (
-	idadministratiu      SERIAL PRIMARY KEY  ,
-	idtreballador        bigint  NOT NULL  ,
-	carg                 text    ,
+CREATE TABLE krankenhaus_schema.administratiu (
+    idadministratiu SERIAL PRIMARY KEY,
+    idtreballador BIGINT NOT NULL,
+    carg TEXT,
     CONSTRAINT fk_administratiu_treballador FOREIGN KEY (idtreballador) REFERENCES krankenhaus_schema.treballador (idtreballador) ON DELETE CASCADE
- );
-
-ALTER TABLE krankenhaus_schema.administratiu ADD CONSTRAINT fk_administratiu_treballador_0 FOREIGN KEY ( idtreballador ) REFERENCES krankenhaus_schema.treballador( idtreballador );
+);
--- src/views.sql
+++ src/views.sql
@@ -1,3 +1,52 @@
 CREATE VIEW getAllWorkers AS
-SELECT nom, cognom FROM persona
-NATURAL JOIN treballador;
+select nom, cognom from persona natural join treballador;
+
+CREATE VIEW getAllPacients AS
+SELECT nom, cognom from persona natural join pacient;
+
+CREATE VIEW getSinEdad AS
+SELECT nom, cognom from persona
+Where datanaixement IS NULL;
+
+CREATE VIEW getMENORES AS
+SELECT nom, cognom from persona
+where datanaixement BETWEEN '2005-05-18' and now();
+
+CREATE VIEW getCapacidadHospital AS
+SELECT idhospital, nomhospital
+FROM hospitales
+WHERE numllitsucilliures>500;
+
+CREATE VIEW getAllHospitals AS
+select nomciutat from ciutat natural right join hospital;
+
+CREATE VIEW getFuturosHospitales AS
+SELECT nomciutat
+FROM ciutat
+WHERE idciutat NOT IN (SELECT idciutat FROM hospital);
+
+CREATE VIEW getEnfermeros AS
+select nom, cognom from persona natural join treballador natural join enfermer;
+
+CREATE VIEW getAdministratius AS
+select nom, cognom from persona natural join treballador natural join administatiu;
+
+CREATE VIEW getDoctores AS
+select nom, cognom from persona natural join treballador natural join doctor;
+(\df + nombre funcion)
+
+CREATE VIEW getStockHospital1 AS
+select SUM(qty) from stock where idhospital='1';
+
+CREATE VIEW getStockHospital2 AS
+select SUM(qty) from stock where idhospital='2';
+
+CREATE VIEW getStockHospital0 AS
+select SUM(qty) from stock where idhospital='0';
+
+CREATE VIEW getVirus AS
+SELECT nomvirus FROM virus
+where numvariant>100000;
+
+
+
