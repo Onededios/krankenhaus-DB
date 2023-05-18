ce3ee032881bd97a513ff6a355cbd6e681a03587
Onededios <joel10olor@gmail.com>
2023-05-18 23:47:42 +0200

    Updated views

--- src/views.sql
+++ src/views.sql
@@ -1,52 +1,89 @@
+-- Cambiar al esquema krankenhaus_schema
+SET search_path TO krankenhaus_schema;
+
+-- Crear la vista getAllWorkers
 CREATE VIEW getAllWorkers AS
-select nom, cognom from persona natural join treballador;
+SELECT nom, cognom
+FROM persona
+NATURAL JOIN treballador;
 
+-- Crear la vista getAllPacients
 CREATE VIEW getAllPacients AS
-SELECT nom, cognom from persona natural join pacient;
+SELECT nom, cognom
+FROM persona
+NATURAL JOIN pacient;
 
+-- Crear la vista getSinEdad
 CREATE VIEW getSinEdad AS
-SELECT nom, cognom from persona
-Where datanaixement IS NULL;
+SELECT nom, cognom
+FROM persona
+WHERE datanaixement IS NULL;
 
+-- Crear la vista getMENORES
 CREATE VIEW getMENORES AS
-SELECT nom, cognom from persona
-where datanaixement BETWEEN '2005-05-18' and now();
+SELECT nom, cognom
+FROM persona
+WHERE datanaixement BETWEEN '2005-05-18' AND NOW();
 
+-- Crear la vista getCapacidadHospital
 CREATE VIEW getCapacidadHospital AS
 SELECT idhospital, nomhospital
-FROM hospitales
-WHERE numllitsucilliures>500;
+FROM hospital
+WHERE numllitsucilliures > 500;
 
+-- Crear la vista getAllHospitals
 CREATE VIEW getAllHospitals AS
-select nomciutat from ciutat natural right join hospital;
+SELECT nomciutat
+FROM ciutat
+NATURAL RIGHT JOIN hospital;
 
+-- Crear la vista getFuturosHospitales
 CREATE VIEW getFuturosHospitales AS
 SELECT nomciutat
 FROM ciutat
 WHERE idciutat NOT IN (SELECT idciutat FROM hospital);
 
+-- Crear la vista getEnfermeros
 CREATE VIEW getEnfermeros AS
-select nom, cognom from persona natural join treballador natural join enfermer;
+SELECT nom, cognom
+FROM persona
+NATURAL JOIN treballador
+NATURAL JOIN enfermer;
 
+-- Crear la vista getAdministratius
 CREATE VIEW getAdministratius AS
-select nom, cognom from persona natural join treballador natural join administatiu;
+SELECT nom, cognom
+FROM persona
+NATURAL JOIN treballador
+NATURAL JOIN administratiu;
 
+-- Crear la vista getDoctores
 CREATE VIEW getDoctores AS
-select nom, cognom from persona natural join treballador natural join doctor;
-(\df + nombre funcion)
+SELECT nom, cognom
+FROM persona
+NATURAL JOIN treballador
+NATURAL JOIN doctor;
 
+-- Crear la vista getStockHospital1
 CREATE VIEW getStockHospital1 AS
-select SUM(qty) from stock where idhospital='1';
+SELECT SUM(qty)
+FROM stock
+WHERE idhospital = '1';
 
+-- Crear la vista getStockHospital2
 CREATE VIEW getStockHospital2 AS
-select SUM(qty) from stock where idhospital='2';
+SELECT SUM(qty)
+FROM stock
+WHERE idhospital = '2';
 
+-- Crear la vista getStockHospital0
 CREATE VIEW getStockHospital0 AS
-select SUM(qty) from stock where idhospital='0';
+SELECT SUM(qty)
+FROM stock
+WHERE idhospital = '0';
 
+-- Crear la vista getVirus
 CREATE VIEW getVirus AS
-SELECT nomvirus FROM virus
-where numvariant>100000;
-
-
-
+SELECT nomvirus
+FROM virus
+WHERE numvariant > 100000;
