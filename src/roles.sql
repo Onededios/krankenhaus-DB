7ec3a1b6f839c23145eeb8a154daedbf560c66fd
Onededios <joel10olor@gmail.com>
2023-05-20 13:39:40 +0200

    Updated roles

--- src/roles.sql
+++ src/roles.sql
@@ -2,29 +2,59 @@ CREATE ROLE pacient;
 GRANT SELECT ON pacient, cartillavacunes, hospital, vacuna TO pacient;
 GRANT CONNECT ON DATABASE krankenhaus TO pacient;
 GRANT USAGE ON SCHEMA krankenhaus_schema TO pacient;
+REVOKE EXECUTE ON FUNCTION ingresaPacient TO pacient;
+REVOKE EXECUTE ON FUNCTION actualizaEstadoPaciente TO pacient;
+REVOKE EXECUTE ON FUNCTION ficaStock TO pacient;
+REVOKE EXECUTE ON FUNCTION posaDosis1 TO pacient;
+REVOKE EXECUTE ON FUNCTION avisaPocStock TO pacient;
+REVOKE EXECUTE ON FUNCTION LlistaPacientsCartilla TO pacient;
 
 CREATE ROLE doctor;
 GRANT SELECT, UPDATE ON pacient, cartillavacunes TO doctor;
 GRANT SELECT ON stock, enfermer TO doctor;
 GRANT CONNECT ON DATABASE krankenhaus TO doctor;
 GRANT USAGE ON SCHEMA krankenhaus_schema TO doctor;
+GRANT EXECUTE ON FUNCTION ingresaPacient TO doctor;
+GRANT EXECUTE ON FUNCTION actualizaEstadoPaciente TO doctor;
+REVOKE EXECUTE ON FUNCTION ficaStock TO doctor;
+GRANT EXECUTE ON FUNCTION posaDosis1 TO doctor;
+GRANT EXECUTE ON FUNCTION avisaPocStock TO doctor;
+REVOKE EXECUTE ON FUNCTION LlistaPacientsCartilla TO doctor;
 
 CREATE ROLE enfermer;
 GRANT SELECT ON pacient, cartillavacunes, doctor, stock TO enfermer;
 GRANT CONNECT ON DATABASE krankenhaus TO enfermer;
 GRANT USAGE ON SCHEMA krankenhaus_schema TO enfermer;
+REVOKE EXECUTE ON FUNCTION ingresaPacient TO enfermer;
+GRANT EXECUTE ON FUNCTION actualizaEstadoPaciente TO enfermer;
+REVOKE EXECUTE ON FUNCTION ficaStock TO enfermer;
+GRANT EXECUTE ON FUNCTION posaDosis1 TO enfermer;
+GRANT EXECUTE ON FUNCTION avisaPocStock TO enfermer;
+REVOKE EXECUTE ON FUNCTION LlistaPacientsCartilla TO enfermer;
 
 CREATE ROLE administratiu;
 GRANT SELECT, UPDATE, INSERT, DELETE ON pacient, cartillavacunes, doctor, enfermer, stock, persona, treballador TO administratiu;
 GRANT SELECT ON hospital, vacuna, virus TO administratiu;
 GRANT CONNECT ON DATABASE krankenhaus TO administratiu;
 GRANT USAGE ON SCHEMA krankenhaus_schema TO administratiu;
+GRANT EXECUTE ON FUNCTION ingresaPacient TO administratiu;
+GRANT EXECUTE ON FUNCTION actualizaEstadoPaciente TO administratiu;
+GRANT EXECUTE ON FUNCTION ficaStock TO administratiu;
+GRANT EXECUTE ON FUNCTION posaDosis1 TO administratiu;
+GRANT EXECUTE ON FUNCTION avisaPocStock TO administratiu;
+GRANT EXECUTE ON FUNCTION LlistaPacientsCartilla TO administratiu;
 
 CREATE ROLE admin;
 GRANT ALL PRIVILEGES ON DATABASE krankenhaus TO admin;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
 GRANT CONNECT ON DATABASE krankenhaus TO admin;
 GRANT USAGE ON SCHEMA krankenhaus_schema TO admin;
+GRANT EXECUTE ON FUNCTION ingresaPacient TO admin;
+GRANT EXECUTE ON FUNCTION actualizaEstadoPaciente TO admin;
+GRANT EXECUTE ON FUNCTION ficaStock TO admin;
+GRANT EXECUTE ON FUNCTION posaDosis1 TO admin;
+GRANT EXECUTE ON FUNCTION avisaPocStock TO admin;
+GRANT EXECUTE ON FUNCTION LlistaPacientsCartilla TO admin;
 
 CREATE USER jrodriguez WITH ENCRYPTED PASSWORD 'passwd' IN ROLE admin;
 
